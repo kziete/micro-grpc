86ce0a973334c804b23a308571db1b3443a37f1d
kziete <sdiaz@talana.com>
2018-11-11 01:13:43 -0300

    todo en teoria, pero no funciona ni media mierda

---
+++ api-gateway/app.py
@@ -0,0 +1,20 @@
+import grpc
+from flask import Flask
+
+import helloworld_pb2
+import helloworld_pb2_grpc
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    with grpc.insecure_channel('flask_service:50051') as channel:
+        stub = helloworld_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+    return response.message
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
